900736f2c0eb55986072b23b2b1cc8c6bde24cd7
yash kumar mishra <mishrayash3778@gmail.com>
2022-07-15 11:59:43 +0530

    some new changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ const Header = props => {
     const {search, onInputChange, onSearchClick }= props;
 
     return(
-       <div className="jumbotron mt-1 bg-rose-900 dark:bg-black p-5">
+       <div className="jumbotron mt-1 bg-rose-900 dark:bg-black p-5 border-2 mb-5 bg-white/10 mt-4  rounded-xl border-white/10 ">
            <h1 className="display-1 text-center  dark:text-white mb-5 font-mono italic font-extrabold " >
 Food Recipes</h1>
   <form onSubmit={onSearchClick}>
-  <div className="input-group  mx-auto">
-  <input type="text" className="form-control bg-gradient-to-r from-rose-900 via-yellow-900 to-green-900 text-white border rounded-1" placeholder="Recipe's name" value={search} onChange={onInputChange} aria-label="Recipient's username" aria-describedby="basic-addon2" />
-  <button type="button" className=" text-black dark:bg-white btn btn-primary dark:hover:bg-red-500 border border-primary rounded-1" onClick={onSearchClick}>Search</button>
+  <div className="input-group ">
+  <input type="text" className="  mx-auto  w-75 bg-white/10 text-black border rounded-1" placeholder="Recipe's name" value={search} onChange={onInputChange} aria-label="Recipient's username" aria-describedby="basic-addon2" />
+  {/* <button type="button" className=" text-black dark:bg-white btn btn-primary dark:hover:bg-red-500 border border-primary rounded-1" onClick={onSearchClick}>Search</button> */}
 </div>
   </form>
   
