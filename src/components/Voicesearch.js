6a06dd56ade7c73ce2e138047127346c1d1dfc8d
yash kumar mishra <mishrayash3778@gmail.com>
2022-07-21 17:55:06 +0530

    color changed og vs

--- src/components/Voicesearch.js
+++ src/components/Voicesearch.js
@@ -29,10 +29,18 @@ const Header = props => {
     return(
         <div className="fixed bottom-20 right-5">
     <div className="border-2 ml-5 w-12 border-white/10 bg-white/25 border-double rounded-full" onClick={voice}>
-<button >  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-mic text-light m-2" viewBox="0 0 16 16">
+<button > 
+  { listening ?
+   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-mic text-red-700 m-2" viewBox="0 0 16 16">
   <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
   <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" />
-</svg></button>
+</svg> :
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-mic text-light m-2" viewBox="0 0 16 16">
+  <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/>
+  <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" />
+</svg>
+}
+</button>
 </div>
 </div>
 
